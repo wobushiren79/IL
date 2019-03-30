65ebec389d689da2c974214f6d9f7ff3e35fd7a2
Apple Coffee <734719526@qq.com>
2019-03-30 20:09:04 +0800

    主界面制作

---
+++ Assets/Scrpit/Component/UI/UIMainCreate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+public class UIMainCreate : BaseUIComponent
+{
+    //返回按钮
+    public Button btBack;
+    public Text tvBack;
+
+    private void Start()
+    {
+        if (btBack != null)
+        {
+            btBack.onClick.AddListener(OpenStartUI);
+        }; 
+    }
+
+    /// <summary>
+    /// 返回开始菜单
+    /// </summary>
+    public void OpenStartUI()
+    {
+        uiManager.OpenUIAndCloseOtherByName("Start");
+    }
+}
---
+++ Assets/Scrpit/Component/UI/UIMainStart.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainStart : BaseUIComponent
+{
+    //开始按钮
+    public Button btStart;
+    public Text tvStart;
+    //继续按钮
+    public Button btContinue;
+    public Text tvContinue;
+    //设置按钮
+    public Button btSetting;
+    public Text tvSetting;
+    //离开按钮
+    public Button btExit;
+    public Text tvExit;
+
+    private void Start()
+    {
+        if (btStart != null)
+        {
+            btStart.onClick.AddListener(OpenCreateUI);
+        }
+        if (btExit != null)
+        {
+            btExit.onClick.AddListener(ExitGame);
+        }
+     
+    }
+
+    public override void RefreshUI()
+    {
+        base.RefreshUI();
+         
+    }
+
+    /// <summary>
+    /// 打开创建页面
+    /// </summary>
+    public void OpenCreateUI()
+    {
+        uiManager.OpenUIAndCloseOtherByName("Create");
+    }
+
+    /// <summary>
+    /// 离开游戏
+    /// </summary>
+    public void ExitGame()
+    {
+        GameUtil.ExitGame();
+    }
+}
