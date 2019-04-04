de485023dd9c5f47f33f9740356e60c83c608284
Apple Coffee <734719526@qq.com>
2019-04-04 21:23:33 +0800

    地图

---
+++ Assets/Scrpit/Component/Manager/Scenes/GameScenesManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class GameScenesManager : BaseManager,IGameDataView
+{
+    public GameDataBean gameData;
+
+    public GameDataController gameDataController;
+
+    private void Awake()
+    {
+        gameDataController = new GameDataController(this,this);
+    }
+
+    private void Start()
+    {
+        gameDataController.GetGameDataByUserId(GameCommonInfo.gameUserId);
+    }
+
+    #region 数据回调
+    public void DeleteGameDataFail()
+    {
+    }
+
+    public void DeleteGameDataSuccess()
+    {
+    }
+
+    public void GetGameDataFail()
+    {
+    }
+
+    public void GetGameDataSimpleListFail()
+    {
+    }
+
+    public void GetGameDataSimpleListSuccess(List<GameDataSimpleBean> listData)
+    {
+    }
+
+    public void GetGameDataSuccess(GameDataBean gameData)
+    {
+        this.gameData = gameData;
+    }
+
+    public void SetGameDataFail()
+    {
+    }
+
+    public void SetGameDataSuccess()
+    {
+    }
+    #endregion
+}
