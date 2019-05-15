e5ec2834bab7cc29e1929a8d3a275048dd58e1ff
Apple Coffee <734719526@qq.com>
2019-05-15 16:17:07 +0800

    出勤表

---
+++ Assets/Scrpit/Component/ListItem/ItemGameAttendanceCpt.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class ItemGameAttendanceCpt : BaseMonoBehaviour
+{
+    public Text tvName;
+    public InfoPromptPopupButton pbName;
+
+    public Text tvPrice;
+    public InfoPromptPopupButton pbPrice;
+
+    public Text tvLoyal;
+    public InfoPromptPopupButton pbLoyal;
+
+    public Text tvSpeed;
+    public InfoPromptPopupButton pbSpeed;
+    public Text tvAccount;
+    public InfoPromptPopupButton pbAccount;
+    public Text tvCharm;
+    public InfoPromptPopupButton pbCharm;
+    public Text tvCook;
+    public InfoPromptPopupButton pbCook;
+    public Text tvForce;
+    public InfoPromptPopupButton pbForce;
+    public Text tvLucky;
+    public InfoPromptPopupButton pbLucky;
+
+    public RadioButtonView rbAttendance;
+
+    public CharacterUICpt characterUICpt;
+    public CharacterBean characterData;
+
+    public void SetData(CharacterBean data)
+    {
+        if (data == null)
+            return;
+        characterData = data;
+        if (characterData.baseInfo != null)
+        {
+            CharacterBaseBean characterBase = characterData.baseInfo;
+            SetName(characterBase.name);
+            SetPrice(characterBase.priceS, characterBase.priceM, characterBase.priceL);
+        }
+        if (characterData.attributes != null)
+        {
+            CharacterAttributesBean characterAttributes = characterData.attributes;
+            SetLoyal(characterAttributes.loyal);
+        }
+        if (data.body != null && data.equips != null)
+            characterUICpt.SetCharacterData(data.body, data.equips);
+    }
+
+
+    /// <summary>
+    /// 设置名字
+    /// </summary>
+    /// <param name="name"></param>
+    public void SetName(string name)
+    {
+        if (tvName == null)
+            return;
+        tvName.text = name;
+    }
+
+    /// <summary>
+    /// 设置工资
+    /// </summary>
+    /// <param name="priceS"></param>
+    /// <param name="priceM"></param>
+    /// <param name="priceL"></param>
+    public void SetPrice(long priceS, long priceM, long priceL)
+    {
+        if (tvPrice == null)
+            return;
+        tvPrice.text = priceS + " / 天";
+    }
+
+    /// <summary>
+    /// 设置忠诚度
+    /// </summary>
+    /// <param name="loyal"></param>
+    public void SetLoyal(float loyal)
+    {
+        if (tvLoyal == null)
+            return;
+        tvLoyal.text = loyal + "";
+    }
+}
---
+++ Assets/Scrpit/Component/UI/UIGameAttendance.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UIGameAttendance : BaseUIComponent
+{
+    public Button btSubmit;
+
+    public GameDataManager gameDataManager;
+
+    public GameObject objListContent;
+    public GameObject objItemWorkModle;
+
+    private void Start()
+    {
+        if (btSubmit != null)
+            btSubmit.onClick.AddListener(StartWork);
+        InitData();
+    }
+
+    public void StartWork()
+    {
+
+    }
+
+    public void InitData()
+    {
+        if (gameDataManager == null)
+            return;
+        List<CharacterBean> listData = new List<CharacterBean>();
+        listData.Add(gameDataManager.gameData.userCharacter);
+        listData.AddRange(gameDataManager.gameData.workCharacterList);
+        for (int i = 0; i < listData.Count; i++)
+        {
+            CharacterBean itemData = listData[i];
+            CreateWorkerItem(itemData);
+        }
+    }
+
+    public void CreateWorkerItem(CharacterBean characterData)
+    {
+        if (objListContent == null || objItemWorkModle == null)
+            return;
+        GameObject objWorkerItem = Instantiate(objItemWorkModle, objListContent.transform);
+        objWorkerItem.SetActive(true);
+        ItemGameAttendanceCpt workerItem = objWorkerItem.GetComponent<ItemGameAttendanceCpt>();
+        if (workerItem != null)
+            workerItem.SetData(characterData);
+    }
+}
