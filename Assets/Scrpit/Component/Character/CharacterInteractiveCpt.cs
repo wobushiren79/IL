da5f3297372da43591a2a53b10ada5bf5699031a
Apple Coffee <734719526@qq.com>
2019-05-19 18:31:44 +0800

    互动

---
+++ Assets/Scrpit/Component/Character/CharacterInteractiveCpt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class CharacterInteractiveCpt : BaseMonoBehaviour
+{
+    public TextMesh tvContent;
+    public SpriteRenderer srIcon;
+
+    public void SetChangeLocation(string location)
+    {
+        gameObject.SetActive(true);
+        tvContent.text = location;
+    }
+
+    public void CloseInteractive()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scrpit/Component/Control/ControlForMoveCpt.cs
+++ Assets/Scrpit/Component/Control/ControlForMoveCpt.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 
 public class ControlForMoveCpt : BaseControl
 {
     //角色移动组建
     public CharacterMoveCpt characterMoveCpt;
+    public GameDataManager gameDataManager;
+    //角色互动
+    public CharacterInteractiveCpt characterInteractiveCpt;
+
+    private void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name.Equals("GameInnScene"))
+        {
+            cameraFollowObj.transform.position = new Vector3(0, 0);
+            characterMoveCpt.minMoveX = -5;
+            characterMoveCpt.maxMoveX = gameDataManager.gameData.GetInnBuildData().innWidth + 5;
+            characterMoveCpt.minMoveY = -5;
+            characterMoveCpt.maxMoveY = gameDataManager.gameData.GetInnBuildData().innHeight + 5;
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -14,14 +31,25 @@ public class ControlForMoveCpt : BaseControl
         float vMove = Input.GetAxis("Vertical");
         if (hMove == 0 && vMove == 0)
         {
-
             characterMoveCpt.StopAnim();
         }
         else
         {
             characterMoveCpt.Move(hMove, vMove);
-        }   
+        }
+        if(transform.position.x<= characterMoveCpt.minMoveX
+            || transform.position.x >= characterMoveCpt.maxMoveX
+              || transform.position.y <= characterMoveCpt.minMoveY
+                || transform.position.y >= characterMoveCpt.maxMoveY)
+        {
+            characterInteractiveCpt.SetChangeLocation("进入小镇");
+        }
+        else
+        {
+            characterInteractiveCpt.CloseInteractive();
+        }
     }
+
     public override void StartControl()
     {
         base.StartControl();
