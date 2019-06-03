32855027c44aa419aa8b064dbc78a10d296f5bce
Apple Coffee <734719526@qq.com>
2019-06-03 18:36:11 +0800

    市场添加

---
+++ Assets/Scrpit/Component/Interactive/InteractivePositionChangeCpt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class InteractivePositionChangeCpt : BaseInteractiveCpt
+{
+    public string interactiveContent;
+    public GameObject positionChangeObj;
+
+    private GameObject mInteractiveObj;
+
+    public override void InteractiveDetection()
+    {
+        if (Input.GetButtonDown("Interactive_E"))
+        {
+            if (mInteractiveObj != null)
+                mInteractiveObj.transform.position = positionChangeObj.transform.position;
+        }
+    }
+
+    public override void InteractiveEnd(CharacterInteractiveCpt characterInt)
+    {
+        this.mInteractiveObj = null;
+        characterInt.CloseInteractive();
+    }
+
+    public override void InteractiveStart(CharacterInteractiveCpt characterInt)
+    {
+        this.mInteractiveObj = characterInt.gameObject;
+        characterInt.ShowInteractive(interactiveContent);
+    }
+}
