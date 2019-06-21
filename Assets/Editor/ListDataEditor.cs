61aaf54040ef3bf694f9a269dd5d7981178593c9
Apple Coffee <734719526@qq.com>
2019-06-21 20:15:41 +0800

    人才市场场景

--- Assets/Editor/ListDataEditor.cs
+++ Assets/Editor/ListDataEditor.cs
@@ -20,7 +20,9 @@ public class ListDataEditor : Editor
     {
         GameObject Target = Selection.gameObjects[0];
         CharacterDressManager dressManager = Target.GetComponent<CharacterDressManager>();
+        dressManager.listIconClothes.Clear();
         AddList("Assets/Texture/Character/character_clothes.png", dressManager.listIconClothes);
+        AddList("Assets/Texture/Character/character_clothes_work.png", dressManager.listIconClothes);
     }
 
     [MenuItem("Custom/List/AddDressShoes")]
@@ -28,7 +30,9 @@ public class ListDataEditor : Editor
     {
         GameObject Target = Selection.gameObjects[0];
         CharacterDressManager dressManager = Target.GetComponent<CharacterDressManager>();
+        dressManager.listIconShoes.Clear();
         AddList("Assets/Texture/Character/character_shoes.png", dressManager.listIconShoes);
+        AddList("Assets/Texture/Character/character_shoes_work.png", dressManager.listIconShoes);
     }
 
     [MenuItem("Custom/List/AddBodyHair")]
@@ -57,7 +61,6 @@ public class ListDataEditor : Editor
 
     public static void AddList(string paths,List<IconBean> listData)
     {
-        listData.Clear();
         string temp = paths;
         Object[] objs = AssetDatabase.LoadAllAssetsAtPath(temp);
         objs.ToList().ForEach(obj =>
