1c2805a8460c8d532c9cf6caebb128e73ba31ace
Apple Coffee <734719526@qq.com>
2019-11-11 20:56:42 +0800

    迷你小游戏 加入爪子和绳索

---
+++ Assets/Scrpit/Component/MiniGame/GameAccount/MiniGameAccountEjectorCpt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MiniGameAccountEjectorCpt : MonoBehaviour
+{
+    public SpriteRenderer srHook;
+    public SpriteRenderer srHookPlatform;
+    public SpriteRenderer srRope;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(srHook&& srHookPlatform&& srRope)
+        {
+            srRope.transform.position = (srHook.transform.position + srHookPlatform.transform.position) / 2f;
+            float angle =  VectorUtil.GetAngle(srHook.transform.position, srHookPlatform.transform.position);
+            float localScale = Vector3.Distance(srHook.transform.position , srHookPlatform.transform.position);
+            srRope.transform.localEulerAngles = new Vector3(0,0, angle+90);
+            srRope.size = new Vector2(0.25f, localScale * 4);
+        }
+    }
+}
