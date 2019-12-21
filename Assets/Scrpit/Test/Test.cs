120a1d5496e54c0b755b4dd954e8ce634ee59105
Apple Coffee <734719526@qq.com>
2019-12-22 04:26:22 +0800

    添加自定义字体

--- Assets/Scrpit/Test/Test.cs
+++ Assets/Scrpit/Test/Test.cs
@@ -5,10 +5,6 @@ using System;
 
 public class Test : BaseMonoBehaviour {
 
-    public int LoopCount = 1000;
-    public int TestCount = 10;
-    public int NumGameObjects = 1000;
-
     private void Awake()
     {
 
