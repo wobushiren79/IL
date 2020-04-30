f79905df7d488eeff2800c9f0cc0479390aa6ce0
Apple Coffee <734719526@qq.com>
2020-04-30 21:11:16 +0800

    山顶场景完善

---
+++ Assets/Scrpit/Component/Element/MoveByGameTimeCpt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MoveByGameTimeCpt : BaseMonoBehaviour
+{
+    protected GameTimeHandler gameTimeHandler;
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float startTimeHour;
+    public float endTimeHour;
+
+    private void Awake()
+    {
+        gameTimeHandler = Find<GameTimeHandler>(ImportantTypeEnum.TimeHandler);
+    }
+
+    private void Update()
+    {
+        if (gameTimeHandler == null)
+            return;
+        gameTimeHandler.GetTime(out float hour, out float min);
+        if (hour >= startTimeHour && hour < endTimeHour)
+        {
+           float lerp = ((hour- startTimeHour) * 60 + min) / ((endTimeHour - startTimeHour) * 60);
+            LogUtil.Log("lerp:"+ lerp);
+           transform.position = Vector3.Lerp(startPosition, endPosition, lerp);
+        }
+    }
+}
