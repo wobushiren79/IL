86292940c852e72da1ec92b5491e84471d895afb
Apple Coffee <734719526@qq.com>
2020-06-08 00:06:33 +0800

    添加一些家具

--- Assets/Editor/ImageEditor.cs
+++ Assets/Editor/ImageEditor.cs
@@ -49,6 +49,12 @@ public class ImageEditor : Editor
     {
         BaseSpriteEditor(SpriteImportMode.Multiple, 5, 2);
     }
+    [MenuItem("Custom/Image/Multiple_5x2_Extrude")]
+    public static void Multiple5x2ForExtrude()
+    {
+        BaseSpriteEditorForExtrude(SpriteImportMode.Multiple, 5, 2);
+    }
+
     [MenuItem("Custom/Image/Multiple_5x3")]
     public static void Multiple5x3()
     {
@@ -61,6 +67,7 @@ public class ImageEditor : Editor
     {
         BaseSpriteEditor(SpriteImportMode.Multiple, 5, 4);
     }
+
     [MenuItem("Custom/Image/Multiple_5x4_Extrude")]
     public static void Multiple5x4ForExtrude()
     {
