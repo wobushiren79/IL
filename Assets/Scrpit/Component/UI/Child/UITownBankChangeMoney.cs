0e5a94db8d9ad51a9d758329dbbcf69e2b79f265
Apple Coffee <734719526@qq.com>
2020-06-10 04:59:18 +0800

    借贷基础

---
+++ Assets/Scrpit/Component/UI/Child/UITownBankChangeMoney.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEditor;
+using DG.Tweening;
+
+public class UITownBankChangeMoney : BaseUIChildComponent<UITownBank>
+{
+    public ItemTownBankExchangeCpt SToM;
+    public ItemTownBankExchangeCpt MToS;
+    public ItemTownBankExchangeCpt MToL;
+    public ItemTownBankExchangeCpt LToM;
+
+    private void Start()
+    {
+        InitData();
+    }
+
+    public override void Open()
+    {
+        base.Open();
+        AnimForInit(SToM.gameObject);
+        AnimForInit(MToS.gameObject);
+        AnimForInit(MToL.gameObject);
+        AnimForInit(LToM.gameObject);
+    }
+
+    public void InitData()
+    {
+        int randomSMRate = 1000;
+        int randomMLRate = 10;
+        //有一定的几率不会有变化幅度
+        Random.InitState(GameCommonInfo.RandomSeed);
+        if (Random.Range(0f, 1f) > 0.9f)
+        {
+            randomSMRate = Random.Range(995, 1005);
+            randomMLRate = Random.Range(9, 11);
+        }
+        //设置汇率
+        if (SToM != null)
+        {
+            SToM.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.SToM, randomSMRate, 1);
+        }
+        if (MToS != null)
+        {
+            MToS.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.MToS, 1, randomSMRate);
+        }
+        if (MToL != null)
+        {
+            MToL.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.MToL, randomMLRate, 1);
+        }
+        if (LToM != null)
+        {
+            LToM.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.LToM, 1, randomMLRate);
+        }
+    }
+
+
+
+    /// <summary>
+    /// 初始化动画
+    /// </summary>
+    /// <param name="obj"></param>
+    public void AnimForInit(GameObject obj)
+    {
+        obj.transform.DOKill();
+        obj.transform.localScale = new Vector3(1, 1, 1);
+        obj.transform.DOScale(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.OutBack).From();
+    }
+}
--- Assets/Scrpit/Component/UI/Town/UITownBank.cs
+++ Assets/Scrpit/Component/UI/Town/UITownBank.cs
@@ -1,67 +1,56 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using DG.Tweening;
-public class UITownBank : UIBaseOne
-{
-    public ItemTownBankExchangeCpt SToM;
-    public ItemTownBankExchangeCpt MToS;
-    public ItemTownBankExchangeCpt MToL;
-    public ItemTownBankExchangeCpt LToM;
+using System;
 
-    private new void Start()
+public class UITownBank : UIBaseOne,IRadioGroupCallBack 
+{
+    public UITownBankChangeMoney uITownBankChange;
+    public UITownBankLoans uITownBankLoans;
+
+    public RadioGroupView rgType;
+
+    public override void Start()
     {
         base.Start();
-        InitData();
+        rgType.SetCallBack(this);
     }
+
     public override void OpenUI()
     {
         base.OpenUI();
-        AnimForInit(SToM.gameObject);
-        AnimForInit(MToS.gameObject);
-        AnimForInit(MToL.gameObject);
-        AnimForInit(LToM.gameObject);
+        rgType.SetPosition(0, false);
+        InitDataByType(StoreForBankTypeEnum.Change);
     }
 
-    public void InitData()
+    private void InitDataByType(StoreForBankTypeEnum type)
     {
-        int randomSMRate = 1000;
-        int randomMLRate = 10;
-        //有一定的几率不会有变化幅度
-        Random.InitState(GameCommonInfo.RandomSeed);
-        if (Random.Range(0f, 1f) > 0.9f)
+        uITownBankLoans.Close();
+        uITownBankChange.Close();
+        switch (type)
         {
-            randomSMRate = Random.Range(995, 1005);
-            randomMLRate = Random.Range(9, 11);
-        }
-        //设置汇率
-        if (SToM != null)
-        {
-            SToM.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.SToM, randomSMRate, 1);
-        }
-        if (MToS != null)
-        {
-            MToS.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.MToS, 1, randomSMRate);
-        }
-        if (MToL != null)
-        {
-            MToL.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.MToL, randomMLRate, 1);
-        }
-        if (LToM != null)
-        {
-            LToM.SetData(ItemTownBankExchangeCpt.ExchangeMoneyEnum.LToM, 1, randomMLRate);
+            case StoreForBankTypeEnum.Change:
+                uITownBankChange.Open();
+                break;
+            case StoreForBankTypeEnum.Loans:
+                uITownBankLoans.Open();
+                break;
         }
     }
 
-    
-
-    /// <summary>
-    /// 初始化动画
-    /// </summary>
-    /// <param name="obj"></param>
-    public void AnimForInit(GameObject obj)
+    #region 回调
+    public void RadioButtonSelected(RadioGroupView rgView, int position, RadioButtonView rbview)
     {
-        obj.transform.DOKill();
-        obj.transform.localScale = new Vector3(1, 1, 1);
-        obj.transform.DOScale(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.OutBack).From();
+        uiGameManager.audioHandler.PlaySound(AudioSoundEnum.ButtonForNormal);
+        StoreForBankTypeEnum type = EnumUtil.GetEnum<StoreForBankTypeEnum>(rbview.name);
+        InitDataByType(type);
     }
+
+
+
+    public void RadioButtonUnSelected(RadioGroupView rgView, int position, RadioButtonView rbview)
+    {
+
+    }
+    #endregion 
 }
