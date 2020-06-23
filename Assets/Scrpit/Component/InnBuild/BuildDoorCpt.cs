b05178c5698e97ca03bdab2484730cfe39a6d65e
Apple Coffee <734719526@qq.com>
2020-06-23 23:16:02 +0800

    添加一些门

--- Assets/Scrpit/Component/InnBuild/BuildDoorCpt.cs
+++ Assets/Scrpit/Component/InnBuild/BuildDoorCpt.cs
@@ -4,7 +4,18 @@ using UnityEditor;
 public class BuildDoorCpt : BaseBuildItemCpt
 {
     public GameObject entranceObj;
-    
+
+    public TextMesh tvInnName;
+    public TextMesh tvInnNameShadow;
+
+    protected GameDataManager gameDataManager;
+    private void Awake()
+    {
+        gameDataManager = Find<GameDataManager>(ImportantTypeEnum.GameDataManager);
+
+
+    }
+
     /// <summary>
     /// 获取入口位置
     /// </summary>
@@ -14,4 +25,21 @@ public class BuildDoorCpt : BaseBuildItemCpt
         return entranceObj.transform.position;
     }
 
+    public override void SetData(BuildItemBean buildItemData)
+    {
+        base.SetData(buildItemData);
+        SetInnName(gameDataManager.gameData.GetInnAttributesData().innName);
+    }
+
+    public void SetInnName(string name)
+    {
+        if (name.Length > 6)
+        {
+            name = name.Substring(0, 6);
+        }
+        if (tvInnName != null)
+            tvInnName.text = name;
+        if (tvInnNameShadow != null)
+            tvInnNameShadow.text = name;
+    }
 }
