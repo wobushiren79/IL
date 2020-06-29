1dd8ff260bcdd94bea9a6c912df2045e629a37b0
Apple Coffee <734719526@qq.com>
2020-06-30 04:54:49 +0800

    自动化生成动画添加

---
+++ Assets/Editor/Base/AnimEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AnimEditor : Editor
+{
+    /// <summary>
+    /// 创建图片的动画
+    /// </summary>
+    /// <param name="itemPicTex"></param>
+    /// <param name="numberForS">1秒多少张</param>
+     public static void CreateAnimForTex(Texture2D itemPicTex,string animPath, int numberForS)
+    {
+        AnimationClip clip = new AnimationClip();
+        //设置帧率为30
+        clip.frameRate = 30;
+
+        //属性参数
+        EditorCurveBinding curveBinding = new EditorCurveBinding();
+        curveBinding.type = typeof(SpriteRenderer);
+        curveBinding.path = "";
+        curveBinding.propertyName = "m_Sprite";
+
+        //帧数相关
+        ObjectReferenceKeyframe[] keyFrames = null;
+
+        string path = AssetDatabase.GetAssetPath(itemPicTex);
+        Object[] objs = AssetDatabase.LoadAllAssetsAtPath(path);
+        keyFrames = new ObjectReferenceKeyframe[objs.Length - 1];
+        //动画长度是按秒为单位，1/10就表示1秒切10张图片，根据项目的情况可以自己调节
+        float frameTime = 1 / (float)numberForS;
+        int i = 0;
+        objs.ToList().ForEach(obj =>
+        {
+            if (obj as Sprite != null)
+            {
+                Sprite itemSprite = obj as Sprite;
+                keyFrames[i] = new ObjectReferenceKeyframe();
+                keyFrames[i].time = frameTime * i;
+                keyFrames[i].value = itemSprite;
+                i++;
+            }
+        });
+
+        //设置idle文件为循环动画
+        AnimationClipSettings clipSetting = AnimationUtility.GetAnimationClipSettings(clip);
+        clipSetting.loopTime = !clip.isLooping;
+        AnimationUtility.SetAnimationClipSettings(clip, clipSetting);
+        //设置参数
+        AnimationUtility.SetObjectReferenceCurve(clip, curveBinding, keyFrames);
+        AssetDatabase.CreateAsset(clip, animPath + "/" + itemPicTex.name + ".anim");
+        AssetDatabase.SaveAssets();
+    }
+
+  
+}
---
+++ Assets/Editor/Game/GameAnimEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class GameAnimEditor : AnimEditor
+{
+
+    [MenuItem("Custom/Anim/CreateAnimForMask")]
+    public static void CreateAnimForMask()
+    {
+        Texture2D[] listText = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D itemPicTex in listText)
+        {
+            CreateAnimForTex(itemPicTex, "Assets/Anim/Animation/Equip/Mask", 10);
+        }
+    }
+
+    [MenuItem("Custom/Anim/CreateAnimForHat")]
+    public static void CreateAnimForHat()
+    {
+        Texture2D[] listText = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D itemPicTex in listText)
+        {
+            CreateAnimForTex(itemPicTex, "Assets/Anim/Animation/Equip/Hat", 10);
+        }
+    }
+
+    [MenuItem("Custom/Anim/CreateAnimForClothes")]
+    public static void CreateAnimForClothes()
+    {
+        Texture2D[] listText = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D itemPicTex in listText)
+        {
+            CreateAnimForTex(itemPicTex, "Assets/Anim/Animation/Equip/Clothes", 10);
+        }
+    }
+
+    [MenuItem("Custom/Anim/CreateAnimForShoes")]
+    public static void CreateAnimForShoes()
+    {
+        Texture2D[] listText = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D itemPicTex in listText)
+        {
+            CreateAnimForTex(itemPicTex, "Assets/Anim/Animation/Equip/Shoes", 10);
+        }
+    }
+
+    [MenuItem("Custom/Anim/CreateAnimForItems")]
+    public static void CreateAnimForItems()
+    {
+        Texture2D[] listText = Selection.GetFiltered<Texture2D>(SelectionMode.DeepAssets);
+        foreach (Texture2D itemPicTex in listText)
+        {
+            CreateAnimForTex(itemPicTex, "Assets/Anim/Animation/Equip/Items", 10);
+        }
+    }
+}
