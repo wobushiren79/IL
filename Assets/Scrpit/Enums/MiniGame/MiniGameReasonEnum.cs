db9cdf50bf1f3aa897ada47228415d33e6918191
Apple Coffee <734719526@qq.com>
2020-07-13 23:59:24 +0800

    添加一些菜谱事件

--- Assets/Scrpit/Enums/MiniGame/MiniGameReasonEnum.cs
+++ Assets/Scrpit/Enums/MiniGame/MiniGameReasonEnum.cs
@@ -2,9 +2,9 @@
 using UnityEditor;
 
 //竞技类型
-public enum MiniGameReasonEnum 
+public enum MiniGameReasonEnum
 {
-    Improve= 1,//晋升
-    Fight=2,//比斗
-    Recruit=3,//招募
+    Improve = 1,//晋升
+    Fight = 2,//比斗
+    Recruit = 3,//招募
 }
