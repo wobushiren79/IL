acc4ce15ff371093b9d47c355e4a67e28212bd79
Joker Apple <734719526@qq.com>
2021-01-17 18:15:41 +0800

    框架修改

--- Assets/Scrpit/Component/Handler/Base/ToastHandler.cs
+++ Assets/Scrpit/Component/Handler/Base/ToastHandler.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class ToastHandler : BaseUIHandler<ToastHandler,ToastManager>
 {
+    public GameObject objToastContainer;
+
     protected override void Awake()
     {
         sortingOrder = 5;
